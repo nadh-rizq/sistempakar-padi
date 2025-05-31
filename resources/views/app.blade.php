825106af96fc907a1c115c5db39a102d213e35c0
nadh-rizq <nadh.rizq@gmail.com>
2025-06-01 00:15:37 +0800

    update head

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
-        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="icon" type="image/x-icon" href="{{ route('show', ['file' => 'favicon.ico']) }}" />
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
